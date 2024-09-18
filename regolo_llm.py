5e2b7a62044e9412195be487507bb6c90812e62d
Piero Savastano <piero.savastano@gmail.com>
2024-09-18 12:55:26 +0200

    cleanup code

--- regolo_llm.py
+++ regolo_llm.py
@@ -3,62 +3,11 @@ from typing import List, Optional, Type, Any, Mapping
 import httpx
 
 from pydantic import ConfigDict
-from langchain_core.language_models.llms import LLM
 from langchain_openai.chat_models import ChatOpenAI
 
 from cat.mad_hatter.decorators import hook
 from cat.factory.llm import LLMSettings
 from cat.log import log
-
-
-# Regolo models settings
-
-
-# class LLMRegolo(LLM):
-#     """Setup for the Regolo plugin"""
-
-#     Regolo_Key: str
-#     model: str = "mistralai/Mistral-7B-Instruct-v0.2"
-
-#     @property
-#     def _llm_type(self) -> str:
-#         return "custom"
-
-#     def _call(self,
-#               prompt: str,
-#               stop: Optional[List[str]] = None,
-#               run_manager: Optional[Any] = None,
-#               **kwargs: Any) -> str:
-        
-#         log.warning(prompt)
-
-#         data = {
-#             "model": self.model,
-#             "messages": [{"role": "user", "content": prompt}]
-#         }
-#         headers = {"Content-Type": "application/json",
-#                    "Accept": "application/json",
-#                    "Authorization": self.Regolo_Key if self.Regolo_Key.__contains__("Bearer") else
-#                    f"Bearer {self.Regolo_Key}"}
-#         try:
-#             response = httpx.post(
-#                 "https://api.regolo.ai/v1/chat/completions",
-#                 headers=headers,
-#                 json=data,
-#                 timeout=httpx.Timeout(timeout=20)
-#             )
-#             json = response.json()
-#             log.warning(json)
-#             generated_text = json["choices"][0]["message"]
-#         except Exception as e:
-#             return str(e)
-#         return generated_text["content"]
-
-#     @property
-#     def _identifying_params(self) -> Mapping[str, Any]:
-#         """Identifying parameters."""
-#         return {"model": self.model, "auth_key": self.Regolo_Key}
-    
 
 class LLMRegolo(ChatOpenAI):
 
@@ -71,24 +20,6 @@ class LLMRegolo(ChatOpenAI):
             streaming=streaming,
             **kwargs
         )
-
-    """
-    def invoke(self, *args, **kwargs):
-        log.warning(args)
-        log.warning(kwargs)
-        out = super().invoke(*args, **kwargs)
-        log.error(out)
-        return out
-
-    """ 
-
-    def _stream(self, *args, **kwargs) -> str:
-        log.warning(args)
-        log.warning(kwargs)
-
-        out = super()._stream(*args, **kwargs)
-        log.error(out)
-        return out
 
 
 def get_models_enum() -> Type[Enum]:
