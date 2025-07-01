b6c5d5d1efac73fa1ee4e9db5564628f472efa18
Fabrizio Elvidio Pardo Bartolomeo <fabrizioelvidio.bartolomeo@gmail.com>
2025-07-01 15:38:41 +0200

    Fixed embedder.

--- regolo_embedder.py
+++ regolo_embedder.py
@@ -1,14 +1,16 @@
 import os
 import json
-import httpx
-from typing import List, Type, Optional
-from enum import Enum
-from cat.mad_hatter.decorators import hook
-from cat.factory.llm import LLMSettings
-from cat.log import log
-from langchain_openai.chat_models import ChatOpenAI
-from pydantic import ConfigDict
 
+from enum import Enum
+from typing import List, Type, Optional
+import httpx
+
+from pydantic import ConfigDict
+from langchain_core.embeddings import Embeddings
+
+from cat.log import log
+from cat.mad_hatter.decorators import hook
+from cat.factory.embedder import EmbedderSettings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,20 +26,36 @@ else:
     if "regolo_key" not in json_settings.keys():
         json_settings["regolo_key"] = ""
 
-class LLMRegolo(ChatOpenAI):
 
-    def __init__(self, model, streaming, **kwargs):
-        super().__init__(
-            model_kwargs={},
-            base_url=os.getenv("REGOLO_BASE"),
-            model_name=model,
-            api_key=json_settings["regolo_key"],
-            streaming=streaming,
-            **kwargs
-        )
+class RegoloEmbeddings(Embeddings):
+    """Regolo embeddings"""
+
+    def __init__(self, model):
+        self.model_name = model
+        self.Regolo_Key = json_settings["regolo_key"]
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        payload = {"input": texts, "model": self.model_name}
+        headers = {"Authorization": self.Regolo_Key if self.Regolo_Key.__contains__("Bearer") else
+        f"Bearer {self.Regolo_Key}"}
+        ret = httpx.post(os.getenv("EMBEDDINGS_URL"), headers=headers, json=payload,
+                         timeout=httpx.Timeout(timeout=int(os.getenv("TIMEOUT"))))
+        ret.raise_for_status()
+        to_return = [e["embedding"] for e in ret.json()["data"]]
+        return to_return
+
+    def embed_query(self, text: str) -> List[float]:
+        payload = {"input": text, "model": self.model_name}
+        headers = {"Authorization": self.Regolo_Key if self.Regolo_Key.__contains__("Bearer") else
+        f"Bearer {self.Regolo_Key}"}
+        ret = httpx.post(os.getenv("EMBEDDINGS_URL"), headers=headers, json=payload,
+                         timeout=httpx.Timeout(timeout=int(os.getenv("TIMEOUT"))))
+        ret.raise_for_status()
+        to_return = ret.json()["data"][0]["embedding"]
+        return to_return
 
 
-def get_models_enum() -> Optional[Type[Enum] | str]:
+def get_embedders_enum() -> Optional[Type[Enum] | str]:
     try:
         # Execute http no cache request
         headers = {
@@ -57,8 +75,8 @@ def get_models_enum() -> Optional[Type[Enum] | str]:
                         {"Authentication Error": "Auth error, please try updating the Api key in the plugin options",
                          "Please try restarting the plugin": "If key is correct try restarting the plugin"})
         elif response.status_code == 503:
-            return Enum("ModelEnum", {"Service unavailable": "Service unavailable",
-                                      "Please try restarting the plugin": "Please try restarting the plugin"})
+            return Enum("Enum", {"Service unavailable": "Service unavailable",
+                                 "Please try restarting the plugin": "Please try restarting the plugin"})
         response.raise_for_status()  # Solleva un'eccezione per errori HTTP
         # Parsing JSON response
         data = response.json()
@@ -67,35 +85,33 @@ def get_models_enum() -> Optional[Type[Enum] | str]:
 
         models_info = data["data"]
         models = {model["model_name"]: model["model_name"] for model
-                  in models_info if model["model_info"]["mode"] == "chat" or model["model_info"]["mode"] is None}
+                  in models_info if model["model_info"]["mode"] == "embedding"}
         if len(models) == 0:
-            return Enum("Enum", {"No models available": "No models available",
-                                 "More models coming soon": "More models coming soon"})
+            return Enum("Enum", {"No embedders available": "No embedders available",
+                                 "More embedders coming soon": "More embedders coming soon"})
         elif len(models) == 1:
-            models["More models coming soon"] = "More models coming soon"
-        return Enum("ModelEnum", models)
-
+            models["More embedders coming soon"] = "More embedders coming soon"
+        return Enum("Enum", models)
     except httpx.RequestError as e:
         raise RuntimeError(f"HTTP request failed: {e}")
     except (KeyError, ValueError) as e:
         raise RuntimeError(f"Invalid response format: {e}")
 
 
-class RegoloLLMSettings(LLMSettings):
-    model: get_models_enum()
-    streaming: bool = True
-    _pyclass: Type = LLMRegolo
+class RegoloEmbeddingsConfig(EmbedderSettings):
+    model: get_embedders_enum()
+    _pyclass: Type = RegoloEmbeddings
 
     model_config = ConfigDict(
         json_schema_extra={
-            "humanReadableName": "Regolo LLM",
-            "description": "LLM on regolo.ai",
+            "humanReadableName": "Regolo embedder models",
+            "description": "Configuration for regolo.ai embeddings",
             "link": f"{os.getenv('REGOLO_URL')}",
         }
     )
 
 
 @hook
-def factory_allowed_llms(allowed, cat) -> List:  # noqa
-    allowed.append(RegoloLLMSettings)
+def factory_allowed_embedders(allowed, cat) -> List:  # noqa
+    allowed.append(RegoloEmbeddingsConfig)
     return allowed
